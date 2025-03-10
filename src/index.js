d0121edce6c552ba63a1b55edf49d47ccaddf7b8
Sanjay Pani <sanjaykpnai@gmail.com>
2025-03-10 21:58:13 +0530

    initila commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//root.render(<h1>Hello</h1>)
+// root.render(
+//     <ul>
+//         <li>Super popular JS library</li>
+//         <li>will help me even more </li>
+//         <li>Cool Logo</li>
+//         <li>D</li>
+//     </ul>
+// )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const reactElement =  <h1>Hello </h1>
+console.log(reactElement);
+
+
+root.render(reactElement);
